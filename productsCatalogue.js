272b3d9869d53ce46bdda93effc29d3b70675311
AlanMFlores <alanmflores.13@gmail.com>
2022-08-10 17:36:16 -0300

    Error de ruta de JSON

--- productsCatalogue.js
+++ productsCatalogue.js
@@ -4,7 +4,7 @@
 
 // Función para obtener los productos del JSON
 const getCatalogueList = async () => {
-    const products = await fetch('./data/products.json');
+    const products = await fetch('../data/products.json');
     const parsedProducts = await products.json();
     saveCatalogueStorage(parsedProducts);
 }
