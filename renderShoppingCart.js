233cd2ae98c62b1b937e924fd7293d2cd59a4118
AlanMFlores <alanmflores.13@gmail.com>
2022-08-14 00:59:15 -0300

    Desarrollando el carrito de compras

--- renderShoppingCart.js
+++ renderShoppingCart.js
@@ -1,37 +1,42 @@
 const shoppingCartContainer = document.querySelector('.shopping-cart--products-list');
-
-
-
+const shoppingCartCheckout = document.querySelector('.shopping-cart');
 
 const renderShoppingCart = () => {
-    let shoppingCartStorage = getShoppingCartStorage();
+    const emptyCart = document.createElement('h3');
+    emptyCart.classList.add('empty-cart')
+    emptyCart.innerHTML = `No hay productos en tu carrito. Puedes agregarlo desde <a href="catalogo.html">AQUÍ</a>`
+    let shoppingCartStorage = getShoppingCartStorage() || [];
 
-    shoppingCartStorage.forEach( product => {
-        let card = document.createElement('li');
-        card.classList.add('shopping-cart--products-list-item')
-    
-        card.innerHTML = `
-                       <article class="shopping-card">
-                            <div class="shopping-card-description">
-                                <figure class="shopping-card--image">
-                                    <img src="../${product.image}" alt="aros-arcilla-redondos-negro">
-                                </figure>
-                                <div class="shopping-card-description--info">
-                                    <h3 class="shopping-card--title">${product.name}</h3>
-                                    <p class="shopping-card-color">Color: <b>Negro</b></p>
-                                    <p class="shopping-card-purchase--amount">Cantidad: <b>1</b></p>
+    if(shoppingCartStorage.length > 0) {
+        shoppingCartStorage.forEach( product => {
+            let card = document.createElement('li');
+            card.classList.add('shopping-cart--products-list-item')
+        
+            card.innerHTML = `
+                           <article class="shopping-card">
+                                <div class="shopping-card-description">
+                                    <figure class="shopping-card--image">
+                                        <img src="../${product.image}" alt="aros-arcilla-redondos-negro">
+                                    </figure>
+                                    <div class="shopping-card-description--info">
+                                        <h3 class="shopping-card--title">${product.name}</h3>
+                                        <p class="shopping-card-color">Color: <b>Negro</b></p>
+                                        <p class="shopping-card-purchase--amount">Cantidad: <b>1</b></p>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="shopping-card-purchase">
-                                <h3 class="shopping-card-purchase--price"><b>$${(product.price).toFixed(2)}</b></h3>
-                            </div>
-                            <div class="shopping-card--delete">
-                                <button class="shopping-card--delete-btn">Eliminar del Carrito</button>
-                            </div> 
-                       </article> `
-
-        shoppingCartContainer.append(card);
-    })
+                                <div class="shopping-card-purchase">
+                                    <h3 class="shopping-card-purchase--price"><b>$${(product.price).toFixed(2)}</b></h3>
+                                </div>
+                                <div class="shopping-card--delete">
+                                    <button class="shopping-card--delete-btn">Eliminar del Carrito</button>
+                                </div> 
+                           </article> `
+    
+            shoppingCartContainer.append(card);
+        })
+    } else {
+        shoppingCartContainer.append(emptyCart);
+    }
 }
 
 renderShoppingCart();
@@ -39,4 +44,15 @@ renderShoppingCart();
 const renderCheckout = () => {
     let checkout = document.createElement('div');
     checkout.classList.add('shopping-cart--checkout')
+    checkout.innerHTML = `
+                        <div class="shopping-cart--checkout">
+                            <p class="shopping-cart--checkout-subtotal">Subtotal: <b>$${reduceCheckout().toFixed(2)}</b></p>
+                            <p class="shopping-cart--checkput-ship">Envío: <b>$${shipCost.toFixed(2)}</b></p>
+                            <h3 class="shopping-cart--checkout-total">Total: <b>$${totalPrice().toFixed(2)}</b></h3>
+                            <button class="shopping-cart--confirm-btn">Confirmar Compra</button>
+                        </div>
+                        `
+    shoppingCartCheckout.append(checkout)
 }
+
+renderCheckout()
--- shoppingCart.js
+++ shoppingCart.js
@@ -1,5 +1,10 @@
 const shoppingCartBtn = document.querySelectorAll('.cart-btn');
 
+function deletProduct() {
+}
+
+const shoppingCardDeleteBtn = document.querySelectorAll('.shopping-card--delete-btn');
+
 let shoppingCartList = [];
 
 const addProductLocalStorage = () => {
@@ -10,6 +15,13 @@ const addProductLocalStorage = () => {
 const addProductShoppingCart = (product) => {
     shoppingCartList.push(product);
     addProductLocalStorage();
+}
+
+// Funcion para eliminar producto del carrito
+
+const removeProductShoppingCart = (product) => {
+    let productToRemove = shoppingCartList.findIndex(elem => elem == product);
+    shoppingCartList.splice(productToRemove, 1);
 }
 
 // Obtener carrito de compras del LocalStorage
@@ -24,3 +36,27 @@ shoppingCartBtn.forEach(item => {
         addProductShoppingCart(matchProduct);
     })
 })
+
+shoppingCardDeleteBtn.forEach(item => {
+    item.addEventListener('click', (event) => {
+        console.log(event.target);
+        removeProductShoppingCart(event.target);
+        renderShoppingCart();
+    })
+})
+
+const reduceCheckout = () => {
+    let shoppingCartListStorage = getShoppingCartStorage();
+    let shoppingCartPriceArr = []
+    shoppingCartListStorage.forEach(price => shoppingCartPriceArr.push(price.price))
+    const subtotalValue = shoppingCartPriceArr.reduce((firstValue, secondValue) => firstValue + secondValue);
+    return subtotalValue;
+    
+}
+
+const shipCost = 600;
+
+const totalPrice = () => {
+    let subtotalCost = reduceCheckout();
+    return shipCost + subtotalCost;
+}
